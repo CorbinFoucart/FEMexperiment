48f8b1c8a5b8d794f4035ff1a41b6216691ecf57
Corbin <foucartc@mit.edu>
2018-06-03 12:38:11 -0400

    updated Mesh1D to include vert count

--- codes/src/msh/mesh_1D.py
+++ codes/src/msh/mesh_1D.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import numpy as np
+
+class Mesh1D(object):
+    def __init__(self, P):
+        """ @param P  vertex points, sorted by x position """
+        self.verts = P
+        self.nElm, self.nVerts, self.nEdges = len(self.verts) - 1, len(self.verts), len(self.verts)
+        self.connectivity = self.build_T()
+        connected_one_side = np.bincount(self.connectivity.ravel()) == 1
+        self.boundary_verts = np.where(connected_one_side)[0]
+
+    def build_T(self):
+        """ element connectivity array from 1D vertex list """
+        T = np.zeros((self.nElm, 2), dtype=int)
+        T[:,0] = np.arange(self.nElm)
+        T[:,1] = np.arange(self.nElm) + 1
+        return T
