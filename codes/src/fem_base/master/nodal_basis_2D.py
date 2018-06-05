d66dc5ea5f7f2a411b242c30ba8789bdea6ba445
Corbin <foucartc@mit.edu>
2018-06-05 15:17:06 -0400

    imported nodal basis class to tutorial

--- codes/src/fem_base/master/nodal_basis_2D.py
+++ codes/src/fem_base/master/nodal_basis_2D.py
@@ -19,21 +19,23 @@ class NodalBasis2DTriangle(NodalBasis2D):
             WARPED:  shifted points for better interpolation behavior (Hesthaven 2008)
         """
         self.p, self.nb = p, int((p+1)*(p+2)/2.)
-        self.basis_domain_verts = MASTER_ELEMENT_VERTICES['TRIANGLE']
+        self.verts = MASTER_ELEMENT_VERTICES['TRIANGLE']
         if nodal_locations == 'UNIFORM':
-            xp, yp = bct.bary2cart(bct.uniform_bary_coords(p))
-            self.nodal_pts = np.vstack((xp, yp).T
+            uniform_bary_coords = bct.uniform_bary_coords(p)
+            xp, yp = bct.bary2cart(self.verts, uniform_bary_coords)
+            self.nodal_pts = np.vstack((xp, yp)).T
 
     def shape_functions_at_pts(self, pts):
         """ computes values of shape functions at pts
         """
-        V = p2d.Vandermonde2D(self.p, self.nodal_pts)
+        V = p2d.Vandermonde2D(self.nodal_pts, self.p)
         VTi = np.linalg.inv(V.T)
-        P_tilde = p2d.P_tilde(pts, p)[:, 0:p]
-        shap = np.dot(VTi, Ptil.T)
+        P_tilde = p2d.P_tilde(pts, self.p)[:, 0:self.nb]
+        shap = np.dot(VTi, P_tilde.T)
         return shap
 
 class NodalBasis2DQuad(NodalBasis2D):
     def __init__(self):
         self.basis_domain_verts = MASTER_ELEMENT_VERTICES['QUAD']
+
 
