5e7c06c7073fe2735d0a85e809cf5611a225a5ea
Corbin <foucartc@mit.edu>
2018-07-08 11:19:32 -0400

    added separate nrmls_2d helper

--- codes/src/fem_maps/fem_map.py
+++ codes/src/fem_maps/fem_map.py
@@ -43,6 +43,10 @@ class Isoparametric_Mapping(FEM_Mapping):
     def __init__(self, master, dgnodes, map_nodes='QUAD'):
         self.Jinv, self._detJ = self.map_fns[map_nodes](master, dgnodes)
 
+    def compute_normals(self):
+        """ computes the element normal vectors """
+        pass
+
     def elm_Jinv(self, elmType, elm): return self.Jinv[elmType][:,:,:,elm]
     def elm_detJ(self, elmType, elm): return self._detJ[elmType][:,elm]
 
@@ -54,10 +58,8 @@ class Affine_Mapping(Isoparametric_Mapping):
     def elm_Jinv(self, elmType, elm): return self.Jinv[elmType][0,:,:,elm]
     def elm_detJ(self, elmType, elm): return self._detJ[elmType][0,elm]
 
-# helpers -- really these should be elsewhere
 def _Jinv_and_detJ(shap_der_list, dgnodes):
     """ computes the inverse Jacobian (Jinv) and detJ transforms at quadrature points
-
     @param shap_der_list list of shape function derivates list by element type
         where shap_der_list[elmType][master elm coord][nodes, n_nbasis_fns] i.e.,
         each element of the top level list is a list of derivative matrices indexed
@@ -65,7 +67,8 @@ def _Jinv_and_detJ(shap_der_list, dgnodes):
         where pts denote the master element points at which the derivatives are
         evaluated at the corresponding physical space point.
     @param dgnodes  list of dgnodes ndarrays arrays
-    @retval detJ  determinants of the Jacobian matrices for each dgnode shape (dof_per_elm, nelm)
+    @retval detJ  determinants of the Jacobian matrices for each dgnode shape
+        (dof_per_elm, nelm)
     """
     dim = dgnodes[0].shape[1]
     Jinv, detJ = list(), list()
@@ -77,10 +80,12 @@ def _Jinv_and_detJ(shap_der_list, dgnodes):
         Jinv.append( J_inplace_inversion_fn(J, etype_detJ) )
     return Jinv, detJ
 
+################################################################################
+# JACOBIAN, JACOBIAN DETERMINANT, INVERSE JACOBIAN COMPUTATIONS
+################################################################################
 def _jacobian(shap_der_at_nodes, nodes):
     """ calculuates the jacobian matrices of an isoparametric transformation at
     the given nodes
-
     @param shap_der_at_nodes  list of derivative matrices indexed by coordinate
         direction, with each entry an ndarray with shape (pts, nb) where pts denote
         the master element points at which the derivatives are evaluated at the
@@ -143,7 +148,41 @@ def _inv_Jacobian_3D(): raise(NotImplementedError)
 inv_J_fns = {1:_inv_Jacobian_1D, 2:_inv_Jacobian_2D, 3:_inv_Jacobian_3D}
 det_J_fns = {1:_detJ_1D, 2:_detJ_2D, 3:_detJ_3D}
 
+################################################################################
+# NORMAL VECTOR COMPUTATIONS
+################################################################################
+
+def _nrmls_1D(): raise(NotImplementedError)
+
+def _nrmls_2D(shap_der, nodes_ed):
+    """ computes 2D normals
+    @param shap_der  list of derivative matrices indexed by coordinate
+        direction, with each entry an ndarray with shape (pts, nb) where pts denote
+        the master element points at which the derivatives are evaluated at the
+        corresponding physical space point.
+    @param nodes  ndarray of physical space nodal points that define each
+        edge shape (ng, dim, nEdges) @retval J List of arrays of Jacobians at each
+        point.
+    NOTE: The derivates are all computed on the master edge, and there is only one
+        differentiation direction.
+    """
+    ng, dim, n_edges = nodes_ed.shape
+    normals = np.zeros_like(nodes_ed)
+    for ed in range(n_edges):
+        derivatives = np.dot(shap_der, nodes_ed[:, :, ed])
+        dxdξ, dydξ  = derivatives[:,0], derivatives[:,1]
+        I = np.sqrt(dxdξ**2 + dydξ**2)
+        nrml = 1/np.abs(I)[:, np.newaxis] * np.vstack((dydξ, -dxdξ)).T
+        normals[:, :, ed] = nrml
+    return normals
+
+def _nrmls_3D(): raise(NotImplementedError)
+
+nrml_fns = {2:_nrmls_2D}
+
+################################################################################
 # graveyard MPU code
+################################################################################
 #elif dim == 3:
 
 #    inv = [sp.zeros(jmt.shape) for jmt in JMT]
